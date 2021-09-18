3614bceb50a7bdb8ecb0f78bba328d048ecd80b2
Nadia Luczywo <nadia.luczywo@unc.edu.ar>
2021-09-18 15:49:49 -0300

    testeo entropy_risso

--- garpar.py
+++ garpar.py
@@ -9,33 +9,36 @@ Created on Wed Sep  1 15:09:17 2021
 
 import numpy as np
 import pandas as pd
-_ENT = 1 / np.log2(2)
+
+_ENT = -1 / np.log2(2)
+
 
 def risso_candidate_entropy(windows_size=4):
+    if windows_size <= 0:
+        raise ValueError("'windows_size' must be > 0")
+
     probability_loss = np.linspace(0.0, 1.0, num=windows_size + 1)
-    
-    # Se corrigen probabilidades porque el cálculo de la entropía trabaja con 
+
+    # Se corrigen probabilidades porque el cálculo de la entropía trabaja con
     # logaritmo y el logaritmo de cero no puede calcularse
     epsilon = np.finfo(probability_loss.dtype).eps
-    probability_loss[0]= epsilon
-    probability_loss[-1]=  1 - epsilon   
+    probability_loss[0] = epsilon
+    probability_loss[-1] = 1 - epsilon
 
-    #Calcula entropy
+    # Calcula entropy
     first_part = probability_loss * np.log2(probability_loss)
-    second_part = (1 - probability_loss)* np.log2 (1 - probability_loss)
+    second_part = (1 - probability_loss) * np.log2(1 - probability_loss)
 
     modificated_entropy = _ENT * (first_part + second_part)
     return modificated_entropy
 
-risso_candidate_entropy(7)
 
-
-#def ngenerator(tm1, random):
+# def ngenerator(tm1, random):
 #    i = random.normal(loc=tm1, scale=0.01)
 #    return tm1 + i
 
 
-#def sim(tn, an, seed, sol):
+# def sim(tn, an, seed, sol):
 #    random = np.random(seed)
 #    rows = []
 #    for t in range(tn):
@@ -51,4 +54,4 @@ risso_candidate_entropy(7)
 #    return pd.DataFrame(rows)
 
 
-#sim(100, 23, 42, ngenerator)
+# sim(100, 23, 42, ngenerator)
---
+++ test_garpar.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+
+import garpar as gp
+
+
+@pytest.mark.parametrize(
+    "windows_size, h",
+    [
+        (1, [0.0, 0.0]),
+        (2, [0.0, 1.0, 0.0]),
+        (3, [0.0, 0.91830, 0.91830, 0.0]),
+        (4, [0.0, 0.81127, 1.0, 0.81127, 0.0]),
+        (5, [0.0, 0.72193, 0.97095, 0.97095, 0.72193, 0.0]),
+        (6, [0.0, 0.65002, 0.91830, 1.0, 0.91830, 0.65002, 0.0]),
+        (7, [0.0, 0.59167, 0.86312, 0.98522, 0.98522, 0.86312, 0.59167, 0.0]),
+    ],
+)
+def test_risso_candidate_entropy(windows_size, h):
+    result = gp.risso_candidate_entropy(windows_size)
+    assert np.allclose(result, h, atol=1e-05)
+
+
+@pytest.mark.parametrize("windows_size", [0, -1])
+def test_risso_candidate_entropy_le0(windows_size):
+    with pytest.raises(ValueError):
+        gp.risso_candidate_entropy(windows_size)
